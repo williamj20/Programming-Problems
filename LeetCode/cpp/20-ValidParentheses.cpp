c84e9026bd643a6ca371226d8d89850671f73346
williamj20 <williamjiang20@gmail.com>
2020-12-23 14:04:51 -0500

    Added solutions

---
+++ LeetCode/cpp/20-ValidParentheses.cpp
@@ -0,0 +1,28 @@
+// Accepted: 12/23/20
+// This solution utilizes the vector structure like a Stack
+
+class Solution {
+public:
+    bool isValid(string s) {
+        vector<char> brackets;
+        for (int i = 0; i < s.size(); i++) {
+            if ((s[i] == '(') || (s[i] == '[') || (s[i] == '{')) {
+                brackets.insert(brackets.begin(), s[i]);
+            }
+            else if ((s[i] == ')') || (s[i] == ']') || (s[i] == '}')) {
+                if (brackets.empty()) {
+                    return false;
+                }
+                else if (((s[i] == ')') && brackets[0] == '(') ||
+                         ((s[i] == ']') && brackets[0] == '[') ||
+                         ((s[i] == '}') && brackets[0] == '{')) {
+                    brackets.erase(brackets.begin());
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+    return brackets.empty();
+    }
+};
